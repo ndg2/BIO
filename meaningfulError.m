7bc85e113adfa8fdaffebe761d2f101364612a3d
ndg2 <ndg2@hw.ac.uk>
2016-11-11 12:25:21 +0000

    Delete meaningfulError.m

--- meaningfulError.m
+++
@@ -1,9 +0,0 @@
-function error = meaningfulError(bestIndividual, nPairs,inputs,nHiddenLayer,HiddenNeurons,DesiredOutputs)
-
-error = 0;
-for i = 1:nPairs
-    error = error + 100*min(1, abs(DesiredOutputs(i)-MLPforward(inputs(i,:)',nHiddenLayer,HiddenNeurons,bestIndividual)/DesiredOutputs(i)));
-end
-
-error = error/nPairs;
-end
