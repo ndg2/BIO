f3c6554eb8df56a737d380f0e1205f71d7c598b4
ndg2 <ndg2@hw.ac.uk>
2016-11-11 12:23:30 +0000

    Delete crossover_inputs.m

--- crossover_inputs.m
+++
@@ -1,9 +0,0 @@
-function individual = crossover_inputs(subPopulation)
-
-indexParents = randperm(length(subPopulation), 2);
-parent1 = subPopulation{indexParents(1)};
-parent2 = subPopulation{indexParents(2)};
-
-individual = (parent1+parent2)/2;
-
-end
